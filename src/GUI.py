8a72087f314cb33a6f0f3b3f2794cdf069ead518
Ewa Janikowska <ewa.janikowska7@gmail.com>
2016-10-30 23:59:04 +0100

    First commit

---
+++ src/GUI.py
@@ -0,0 +1,71 @@
+import wx
+import NeuralNetwork
+
+class MyFrame(wx.Frame):
+    posx1 = None
+    posy1 = None
+    posy2 = None
+    panel = None
+    isPainting = False
+    points = [] 
+    def __init__(self):
+        wx.Frame.__init__(self, None, title='Aproksymacja funkcji dwuwartosciowej')
+        panel = wx.Panel(self, -1)
+        panel.Bind(wx.EVT_LEFT_DOWN, self.onLeftClick)
+        panel.Bind(wx.EVT_RIGHT_DOWN, self.onRightClick)
+        panel.Bind(wx.EVT_PAINT, self.on_paint)
+        panel.Bind(wx.EVT_MOTION, self.on_move)
+        self.nn = NeuralNetwork.NeuralNetwork([1,10,10,2], 0, self.GetSize().GetWidth())
+
+    def onLeftClick(self, event):
+        pos = event.GetPosition()
+        self.posx1 = pos.x
+        self.posy1 = pos.y
+        self.isPainting = True
+
+    def onRightClick(self, event):
+        if self.isPainting is True:
+            self.isPainting = False
+            pos = event.GetPosition()
+            self.posy2 = pos.y
+            self.points.append([self.posx1, self.posy1, self.posy2])
+            self.nn.train(self.points)
+            self.posx1 = self.posy1 = self.posy2 = None
+            self.Refresh()
+
+    def on_paint(self, event):
+        dc = wx.PaintDC(event.GetEventObject())
+        dc.SetPen(wx.Pen('blue', 1, wx.PENSTYLE_DOT))
+        if self.posx1 is not None:
+            dc.DrawLine(self.posx1, 0, self.posx1, self.GetSize().GetHeight())
+        if self.posx1 is not None and self.posy1 is not None:
+            dc.SetPen(wx.Pen('blue'))
+            dc.DrawCircle(self.posx1, self.posy1, 2)
+        if self.posx1 is not None and self.posy2 is not None:
+            dc.SetPen(wx.Pen('red'))
+            dc.DrawCircle(self.posx1, self.posy2, 2)
+        for val in self.points:
+            dc.SetPen(wx.Pen('blue'))
+            dc.DrawCircle(val[0], val[1], 2)
+            dc.SetPen(wx.Pen('red'))
+            dc.DrawCircle(val[0], val[2], 2)
+        if len(self.points) > 0:
+            points = self.nn.predict(list(range(self.GetSize().GetWidth())))
+            for i, val in enumerate(points):
+                dc.SetPen(wx.Pen('blue'))
+                dc.DrawPoint(i, val[0])
+                dc.SetPen(wx.Pen('red'))
+                dc.DrawPoint(i, val[1])
+
+    def on_move(self, event):
+        if self.isPainting is True:
+            pos = event.GetPosition()
+            self.posy2 = pos.y
+            self.Refresh()
+
+
+if __name__ == '__main__':
+    app = wx.App(False)
+    frame = MyFrame()
+    frame.Show(True)
+    app.MainLoop()
---
+++ src/NeuralNetwork.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+class NeuralNetwork:
+    def __init__(self, layers, minimum, maximum):
+        self.minimum = minimum
+        self.maximum = maximum
+        self.no_of_layers = len(layers)
+        np.random.seed(1)
+        self.weights = [
+                (2 * np.random.random((layers[i],val)) - 1) 
+                for i, val in enumerate(layers[1:])]
+
+    def sigmoid(self, x):
+        return 1/(1+np.exp(-x))
+
+    def sigmoid_deriv(self, x):
+        return x*(1-x)
+    
+    def normalize(self, data):
+        return (data - self.minimum) / (self.maximum - self.minimum)
+
+    def denormalize(self, data):
+        return (data * (self.maximum - self.minimum)) + self.minimum
+
+    def train(self, dataset):
+        n = self.no_of_layers
+        dataset = self.normalize(np.asarray(dataset, dtype=float))
+        layers = [None] * n
+        l_error = [None] * n
+        l_delta = [None] * n
+        for j in xrange(10000):
+            layers[0] = np.asarray([dataset.T[0]]).T
+            for i in xrange(1, n):
+                layers[i] = self.sigmoid(np.dot(layers[i-1], self.weights[i-1]))
+
+            l_error[n-1] = dataset[:,1:3] - layers[n-1]
+            for i in xrange(n-1, 1, -1):
+                l_delta[i] = l_error[i] *self.sigmoid_deriv(layers[i])
+                l_error[i-1] = l_delta[i].dot(self.weights[i-1].T)
+            l_delta[1] = l_error[1] * self.sigmoid_deriv(layers[1])
+
+            for i in xrange(n-2, -1, -1):
+                self.weights[i] += layers[i].T.dot(l_delta[i+1])
+        
+    def predict(self, dataset):
+        dataset = self.normalize(np.asarray(dataset, dtype=float))
+        result = []
+        layers = [None] * self.no_of_layers
+        for val in dataset:
+            layers[0] = np.asarray(float(val))
+            for i in xrange(1, self.no_of_layers):
+                layers[i] = self.sigmoid(np.dot(layers[i-1], self.weights[i-1]))
+            result.append(layers[self.no_of_layers-1][0])
+        return self.denormalize(np.asarray(result)).tolist()
+
+
